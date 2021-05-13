daf2825906b29f88d929e56d03ff5e8ecf2077a8
Darkskull Dawn Zenith <darknycoilermlp@gmail.com>
2021-05-13 16:35:16 -0300

    Alright then, let's see this now...

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -778,8 +778,9 @@ class PlayState extends MusicBeatState
 						stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
 						stageCurtains.antialiasing = true;
 						stageCurtains.scrollFactor.set(0.1, 0.1);
-						FlxTween.angle(stageCurtains, {angle: 360.0}, 5, {
-							ease: FlxEase.smootherStepIn
+						FlxTween.angle(stageCurtains, {angle: 360}, 5, {
+							ease: FlxEase.smootherStepIn,
+							onComplete: function(twn:FlxTween)
 						});
 
 						add(stageCurtains);
