478ebbe18baffaef8b40afe3a2db8211ed4c9b62
Darkskull Dawn Zenith <darknycoilermlp@gmail.com>
2021-05-30 23:20:44 -0300

    Yeah yeah yeah...

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -57,7 +57,7 @@ class DialogueBox extends FlxSpriteGroup
 		add(bgFade);
 
 		
-		if (gaming)
+		if (pixel)
 		{
 			bgFade = new FlxSprite(-200, -200).makeGraphic(Std.int(FlxG.width * 1.3), Std.int(FlxG.height * 1.3), 0xFFB3DFd8);
 			bgFade.scrollFactor.set();
